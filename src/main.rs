be4bf37a35c6d0e7f9e03552e70aa39b64a74ab2
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-04-27 17:43:35 -0500

    rust-gdb testing

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ mod instructions;
 mod push;
 
 fn main() {
+    let tvec = vec![1, 2, 3, 4, 5];
+    println!("{:?}", tvec);
+
     // These need to stay so linter doesn't go crazy.
     let mut empty_state = EMPTY_STATE;
+    empty_state.int = vec![1, 2, 3];
     interpret_program(&mut empty_state, 1000, 1000);
 
     int_instructions();
