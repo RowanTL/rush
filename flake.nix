7dcbd31a00ed1d609e75500969448af628efe73e
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-04-28 12:15:00 -0500

    nightly devshell

--- flake.nix
+++ flake.nix
@@ -35,6 +35,29 @@
             export SHELL=${pkgs.lib.getExe pkgs.bashInteractive}
           '';
         };
+        devShells.nightly = with pkgs; mkShell {
+          buildInputs = [
+            # rust-bin.beta.latest.default
+            rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
+          ];
+          packages = with pkgs; [
+            # rust-bin.beta.latest.clippy
+            # rust-bin.beta.latest.rust-analyzer
+            # rust-bin.beta.latest.rustfmt
+            # rust-bin.beta.latest.rust-std
+            rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [ "clippy" "rust-analyzer" "rustfmt" "rust-std" ];
+              # targets = [ "arm-unknown-linux-gnueabihf" ];
+            })
+            gdb
+            # gdb-dashboard
+            bacon
+            python313Packages.pygments # for personal gdb-dashboard use
+          ];
+          shellHook = ''
+            export SHELL=${pkgs.lib.getExe pkgs.bashInteractive}
+          '';
+        };
       }
     );
 }
